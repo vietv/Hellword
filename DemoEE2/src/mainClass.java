3da65529f21ec1156de3b658eab3331fb2dd95bc
vietv <viet.vq@big-labs.com>
2014-02-24 15:37:19 +0700

    New code

---
+++ DemoEE2/src/mainClass.java
@@ -0,0 +1,13 @@
+
+public class mainClass {
+
+	public mainClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world 1");
+	}
+
+}
